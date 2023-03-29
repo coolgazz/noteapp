1f2ad68a49df05be4ccdda72f4ed149abf81bb4a
Frost_Feng <kagecgc@gmail.com>
2023-03-29 15:04:37 +0300

    commit2

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,4 @@
 import '../scss/style.scss' 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB_Evp2sJAgl_lxEzIcgEWCfVqhE8LUasY",
-  authDomain: "notes-app-a51f6.firebaseapp.com",
-  projectId: "notes-app-a51f6",
-  storageBucket: "notes-app-a51f6.appspot.com",
-  messagingSenderId: "12355516081",
-  appId: "1:12355516081:web:124007eae75e943bf89490",
-  measurementId: "G-XRP5TZQCCN"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
 
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
